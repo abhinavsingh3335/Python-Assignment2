77e9ccf58be728ef36161f6f38087ba7332fa59a
Abhinav Singh <singhabhinavrajput01@gmail.com>
2025-08-07 16:45:55 +0530

    Added Task1 and Task2 script of Assignment2nd
    Task 1: Check if a Number is Even or Odd
    Task 2: Sum of Integers from 1 to 50 Using a Loop

---
+++ ASS2.py
@@ -0,0 +1,14 @@
+
+#Task 1
+a = int(input('Enter the Number:'))
+if a%2==0:
+    print(+a,'is an Even Number')
+else:
+    print(+a,'is an Odd Number')
+
+#Task 2
+sum = 0
+for i in range(1, 51):
+    sum += i
+
+print('the sum of number:', sum)
